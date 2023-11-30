1d6c4880b00032ac39eb979b7c7616fedf10c2a9
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-11-29 20:41:04 -0600

    Setup Store

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
 // components
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
 // items
 import cartItems from './data';
+
 // redux stuff
+import { legacy_createStore as createStore } from 'redux';
+
+// reducer - function to update the store/state
+const reducer = () => {
+  console.log('Hello world');
+};
+
+// store - stores the data, think of state (of the app)
+const store = createStore(reducer);
 
 function App() {
   // cart setup
