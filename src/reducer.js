9060f2062d065b2180c480bd94e3f4cc7a4ee39b
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-12-03 00:42:32 -0600

    Refactor Decrease

--- src/reducer.js
+++ src/reducer.js
@@ -25,19 +25,12 @@ const reducer = (state, action) => {
     };
   }
   if (action.type === DECREASE) {
-    let tempCart = [];
-    if (action.payload.amount === 1) {
-      tempCart = state.cart.filter((cartItem) => {
-        return cartItem.id !== action.payload.id;
-      });
-    } else {
-      tempCart = state.cart.map((cartItem) => {
-        if (cartItem.id === action.payload.id) {
-          cartItem = { ...cartItem, amount: cartItem.amount - 1 };
-        }
-        return cartItem;
-      });
-    }
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload.id) {
+        cartItem = { ...cartItem, amount: cartItem.amount - 1 };
+      }
+      return cartItem;
+    });
     return {
       ...state,
       cart: tempCart,
